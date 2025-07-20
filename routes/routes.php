3577f279e023726520b12c2f744fc041d50094b8
Mauricio Hernández <mhernandez@dragontec.net>
2025-07-20 11:20:52 -0600

    Actualizacion controladores y modelos
    Se agregan clases y objetos para hacer la peticion a la base de datos de manera dinamica.
    Se podra consultar la tabla y las columnas podemdio de parametros get

--- routes/routes.php
+++ routes/routes.php
@@ -1,64 +1,60 @@
-<?php
+    <?php
 
 
-$routesArray = explode("/", ($_SERVER['REQUEST_URI']));
-$routesArray = array_filter($routesArray);
-//echo '<pre>'; print_r($routesArray); echo '</pre>';
+    $routesArray = explode("/", ($_SERVER['REQUEST_URI']));
+    $routesArray = array_filter($routesArray);
+    //echo '<pre>'; print_r($routesArray); echo '</pre>';
 
 
-//Cuando no se hace ninguna peticion a la API se ejcuta la siguiente condicion
+    //Cuando no se hace ninguna peticion a la API se ejcuta la siguiente condicion
 
-if(count($routesArray) == 0){
-
-
-        $json = array(
-            'status' => 400,
-            'result' => 'Not found'
-                    );
-
-    echo json_encode($json, JSON_PRETTY_PRINT, http_response_code($json["status"]));   
-    return ; 
-}
+    if(count($routesArray) == 0){
 
 
-/* Cuando si se hace una peticion a la api  */
-if(count($routesArray) == 1 && isset($_SERVER['REQUEST_METHOD'])){
+            $json = array(
+                'status' => 400,
+                'result' => 'Not found'
+                        );
 
-    /* Periticiones GET */
-
-    if($_SERVER['REQUEST_METHOD'] == "GET"){
-        $json = array(
-            'status' => 200,
-            'result' => 'Solicitud GET'
-                    );
-        echo json_encode($json, JSON_PRETTY_PRINT, http_response_code($json["status"]));                       
+        echo json_encode($json, JSON_PRETTY_PRINT, http_response_code($json["status"]));   
+        return ; 
     }
-    /*Peticion POST */
-        if($_SERVER['REQUEST_METHOD'] == "POST"){
-        $json = array(
-            'status' => 200,
-            'result' => 'Solicitud POST'
-                    );
-        echo json_encode($json, JSON_PRETTY_PRINT, http_response_code($json["status"]));                       
+
+
+    /* Cuando si se hace una peticion a la api  */
+    if(count($routesArray) == 1 && isset($_SERVER['REQUEST_METHOD'])){
+
+        /* Periticiones GET */
+
+        if($_SERVER['REQUEST_METHOD'] == "GET"){
+                include "services/get.php";           
+        }
+        /*Peticion POST */
+            if($_SERVER['REQUEST_METHOD'] == "POST"){
+            $json = array(
+                'status' => 200,
+                'result' => 'Solicitud POST'
+                        );
+            echo json_encode($json, JSON_PRETTY_PRINT, http_response_code($json["status"]));                       
+        }
+        /*Peticion PUT */
+            if($_SERVER['REQUEST_METHOD'] == "PUT"){
+            $json = array(
+                'status' => 200,
+                'result' => 'Solicitud PUT'
+                        );
+            echo json_encode($json, JSON_PRETTY_PRINT, http_response_code($json["status"]));                       
+        }    
+        /*Peticion DELETE */
+            if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+            $json = array(
+                'status' => 200,
+                'result' => 'Solicitud DELETE'
+                        );
+            echo json_encode($json, JSON_PRETTY_PRINT, http_response_code($json["status"]));                       
+        }    
+        
+
     }
-    /*Peticion PUT */
-        if($_SERVER['REQUEST_METHOD'] == "PUT"){
-        $json = array(
-            'status' => 200,
-            'result' => 'Solicitud PUT'
-                    );
-        echo json_encode($json, JSON_PRETTY_PRINT, http_response_code($json["status"]));                       
-    }    
-    /*Peticion DELETE */
-        if($_SERVER['REQUEST_METHOD'] == "DELETE"){
-        $json = array(
-            'status' => 200,
-            'result' => 'Solicitud DELETE'
-                    );
-        echo json_encode($json, JSON_PRETTY_PRINT, http_response_code($json["status"]));                       
-    }    
-    
 
-}
-
-?>
+    ?>
