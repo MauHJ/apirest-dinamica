443f4978d4ea66ec2efa3ab93f286908e81b8c6b
Mauricio Hernández <mhernandez@dragontec.net>
2025-07-19 19:22:39 -0600

    Se agrega archivo htaccess
    Archivo de seguridad para que no puedan hacer peticiones directas

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
-<?
+<?php
+
+
+//Mostrar errores
+
+
+ini_set('Display_errors',1);
+ini_set("log_errors",1);
+ini_set("error_log","C:\laragon\www\apirest-dinamica/php_error_log");
+
 
 
 require_once "controller/routes.controller.php";
 
 $index = new RoutesController();
-
+$index -> index();
